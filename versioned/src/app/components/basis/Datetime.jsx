e5453f1d90dc62d4bb531790af06cda70fb621c4
charly reux <charlyreux@gmail.com>
2024-04-07 13:39:20 +0200

    Remove unused files and code, and update project

--- versioned/src/app/components/basis/Datetime.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Text } from 'evergreen-ui'
-
-const DateTime = ({ value }) => <Text>{ new Date(value).toLocaleString('en-US') }</Text>
-
-export default DateTime
--- versioned/src/app/domain/Task.js
+++
@@ -1,4 +0,0 @@
-export const TaskTypes = {
-  UserStory: 'UserStory',
-  TechnicalStory: 'TechnicalStory',
-}
